e471f4ad6e70e046210adbdbbdad570c87adb13c
Jorge Luís <56854357+yurixquest@users.noreply.github.com>
2019-11-14 11:42:39 -0300

    Update Aula 1 - Operadores aritméticos.py
    Atualizando descrições e melhorando a escrita

--- Aula 1 - Operadores aritméticos.py
+++ Aula 1 - Operadores aritméticos.py
@@ -1,8 +1,9 @@
-# Os operadores normalmente utilizados são '+' para adição '-' para subtração, '*' para multiplicação '/' para divisão
-# '**' para potencia, '//' para divisão inteira e '%' para módulo ou resto dda divisão
-# Antes do Operador aritimético necessita-se do operando, no caso números. Estes operadore são binários, ou seja, necessitam de outros
-# operando para concluir a operação, no caso, numeros após os simbolos. Para concluir as operações são atribuídos dois simbolos de =, pois apenas um indica
-# correspondencia e não resultado.
+'''   Os operadores normalmente utilizados são '+' para adição '-' para subtração, '*' para multiplicação '/' para divisão
+'**' para potência, '//' para divisão inteira e '%' para módulo ou resto da divisão.
+Antes do Operador aritimético necessita-se do operando, no caso números ou as variáveis que representam os números . Estes operadore são
+binários, ou seja, necessitam de outros operandos para concluir a operação, no caso, numeros após os simbolos. Para concluir as operações
+são atribuídos dois simbolos de =, pois, quando se usa apenas um, indica-se correspondencia e não resultado.'''
+
 5 + 2 == 7
 5 - 2 == 3
 5 * 2 == 10
@@ -11,8 +12,9 @@
 5 // 2 == 2 #Divisão sem usar virgulas
 5 % 2 == 1 #vALOR QUE SOBRA DA DIVISÃO INTEIRA (vizualizado no quociente)
 
-# Os operadores possuem ordem de precedência (como na matemática), as primeiras coisas a serem executadas são os numéros dentro dos parenteses (podem haver parenteses dentro de parenteses
-# Após os parenteses, o que se é resolvido em sequência são as potências -> a multiplciação, divisão, divisão inteira e resto da divisão -> por último adição e subtração
+'''Os operadores possuem ordem de precedência (como na matemática), os primeiros a serem executadas são os numéros dentro dos 
+parenteses (podem haver parenteses dentro de parenteses). Após os parenteses, o que se é resolvido em sequência são as potências -> 
+a multiplciação, divisão, divisão inteira e resto da divisão -> por último adição e subtração.'''
 
 a = 5
 b = 2
@@ -55,15 +57,15 @@ c = 81
 d = c **(1/2) #1/3 para raíz cúbica
 print('Raíz quadrada', d)
 
-# É possível operar strings também, veja exemplos:
+# É possível operar strings também, exemplos:
 print('='*20)
 print('Ola'*3)
 
 # Alguns detalhes como a centralização de respostas pode ser útil, para isto segue o exemplo:
 nome = input('Qual o seu nome?')
-print('Prazer em te conhecer {:>20}!'.format(nome))
-print('Prazer em te conhecer {:<20}!'.format(nome))
-print('Prazer em te conhecer {:^20}!'.format(nome))
+print('Prazer em te conhecer {:>20}!'.format(nome)) #Direita
+print('Prazer em te conhecer {:<20}!'.format(nome)) #Esquerda
+print('Prazer em te conhecer {:^20}!'.format(nome)) #Centro
 print('Prazer em te conhecer {:=^20}!'.format(nome))
 
 # Agora com números
@@ -73,12 +75,12 @@ s = n1 + n2
 n = n1 * n2
 w = n1 / n2
 z = n1 ** n2
-print('A soma vale {}'.format(n1+n2)) #caso só vá usar esses números uma ou poucas vezes
-print('A soma é {}, o produto é {}, a divisão é {}, e a potência é {}'.format(s, n, w, z)) #Dar variaveis aos resultados para usar os mesmos várias vezes no script
+print('A soma vale {}'.format(n1+n2)) #quando não atribuímos variáveis à operação são casos em que iremos utiliza-los uma ou poucas vezes
+print('A soma é {}, o produto é {}, a divisão é {}, e a potência é {}'.format(s, n, w, z)) #Dar variaveis aos resultados auxiliam a usar os mesmos números várias vezes no script
 
-# Caso queira escolher as casas decimas que serão mostradas, dentro dos {} digitar {:.3f} que significa 3 casas flutuantes, e o númeor pode ser variavel para qtd de casas desejadas
-# A sintaxe com virgulas ou com "+' sao menos poderosas que com {} pois o {} da margem a outras configurações como visto acima
-# Caso os resultados sejam para aparecerem todos na mesma linha (não quebrar linhas) digitar end=' ', tal como:
+''' Caso queira escolher as casas decimas que serão mostradas, dentro dos {} digitar {:.3f} que significa 3 casas flutuantes, e o número pode ser variavel para quantidade de casas desejadas
+A sintaxe com virgulas ou com "+' sao menos poderosas que com {} pois o {} possibilita outras configurações como visto acima. Caso os resultados devam aparecer todos na mesma linha 
+(não quebrar linhas) digitar end=' ', tal como:'''
 
 print('A soma vale {}, o produto vale {}, a divisão vale {}, e a potência vale {}'.format(s, n, w, z), end=' ') # O espaço vazio entre as apsas simples do laod de end= significa o que será colocado para juntar as linhas
 
